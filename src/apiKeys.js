0a2ea8a2fa1736757f61db8c46f5cbd59fa3a9ce
Abhishek Chatruvedi <abhishekchatruvedi291@gmail.com>
2023-08-21 01:04:10 +0530

    initial commit

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "91eee113518725d3939a6c71209b56f3",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
